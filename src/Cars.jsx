eb943029574986127fdd272e63b2a86ea6a01406
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-01-08 17:34:32 -0300

    tentativa

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,61 +1,57 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
+const Cars = () => {
+  const { cars, moveCar } = useContext(CarsContext);
+  const { red, blue, yellow } = cars;
+  return (
+    <div>
       <div>
-        <div>
-          <img
-            className={redCar ? 'car-right' : 'car-left'}
-            src={carRed}
-            alt="red car"
-          />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={blueCar ? 'car-right' : 'car-left'}
-            src={carBlue}
-            alt="blue car"
-          />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={yellowCar ? 'car-right' : 'car-left'}
-            src={carYellow}
-            alt="yellow car"
-          />
-          <button
-            onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
+        <img
+          className={red ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          onClick={() => moveCar('redCar', !red)}
+          type="button"
+        >
+          Move
+      </button>
       </div>
-    )
-  }
-};
-
-Cars.contextType = CarsContext;
+      <div>
+        <img
+          className={blue ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          onClick={() => moveCar('blueCar', !blue)}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+      <div>
+        <img
+          className={yellow ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button
+          onClick={() => moveCar('yellowCar', !yellow)}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+    </div>
+  )
+}
 
 export default Cars;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,49 +1,28 @@
 // src/context/Provider.js
-
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
+const Provider = ({ children }) => {
+  const [cars, setCars] = useState({
+    red: false,
+    blue: false,
+    yellow: false
+  });
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
+  const moveCar = (car, side) => {
+    setCars({
+      ...cars,
+      [car]: side,
     });
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
+  const context = { cars, moveCar };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
-};
-
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  return (
+    <CarsContext.Provider value={context}>
+      {children}
+    </CarsContext.Provider>
+  );
+}
 
 export default Provider;
